44243ce3b17fd9d41bbe272083c2e40768e60cd1
Christopher Demahy <demahyc@gmail.com>
2022-03-02 22:00:27 +0000

    testing gitpod

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 // import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -12,6 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className="text-4xl text-blue-500">Hello World</h1>
+      oof
     </>
   );
 };
